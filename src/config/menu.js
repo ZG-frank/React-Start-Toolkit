befd5b1900fed878c84429b8934c429539315fa3
ZG-frank <zzgwhu2015@163.com>
2018-10-13 23:39:15 +0800

    [init] add routes menu config

---
+++ src/config/menu.js
@@ -0,0 +1,24 @@
+export default [{
+    key   : '/dashboard',
+    title : 'Dashboard',
+    icon  : 'dashboard'
+}, {
+    key   : '/postManage',
+    title : 'Posts',
+    icon  : 'read'
+}, {
+    key   : '/userManage',
+    title : 'Users',
+    icon  : 'team',
+    list  : [{
+        key   : '/userList',
+        title : '用户列表'
+    }, {
+        key   : '/userEdit',
+        title : '用户管理'
+    }]
+}, {
+    key   : '/tagManage',
+    title : 'Tags',
+    icon  : 'read'
+}]
