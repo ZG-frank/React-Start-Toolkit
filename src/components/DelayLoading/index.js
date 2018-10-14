e59bda4482cfa2bafa58c9f0d57277d0e9228407
ZG-frank <zzgwhu2015@163.com>
2018-10-14 23:50:49 +0800

    [update] loading

--- src/components/DelayLoading/index.js
+++ src/components/DelayLoading/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 const  DelayLoading = (props) => {
-    console.log(props)
     if (props.error) {
         return <div>Sorry, there was a problem when loading the page.</div>;
     } else if (props.pastDelay) {
