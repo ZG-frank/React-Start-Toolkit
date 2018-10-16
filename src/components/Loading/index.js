765403c8dc5ae5520c83f922468dbe2098e466e8
ZG-frank <zzgwhu2015@163.com>
2018-10-17 01:24:05 +0800

    [update] loading style

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
-import { bindActionCreators } from 'redux'
-import { withRouter } from 'react-router-dom'
-import { connect } from 'react-redux'
-import { Spin, Icon } from 'antd'
-import * as ROOT_action from '@/store/ROOT/action'
-import './index.less'
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Spin } from 'antd';
+import * as ROOT_action from '@/store/ROOT/action';
+import './index.less';
 
 @withRouter
 @connect(
@@ -15,26 +15,24 @@ class Loading extends Component {
     
     componentDidUpdate() {
         if (this.props.ROOT_loading) {
-            this.props.ROOT_DelayLoading()
+            this.props.ROOT_DelayLoading();
         }
     }
 
     componentWillReceiveProps(nextProps) {
         if (this.props.location.pathname != nextProps.location.pathname) {
-            this.props.ROOT_ChangeLoading(true)
+            this.props.ROOT_ChangeLoading(true);
         }
     }
 
     render() {
-        const { ROOT_loading } = this.props
-        const antIcon = <Icon type="loading" style={{ fontSize: 24 }} spin />;
-
         return (
             <Spin 
-                tip='LOADING' 
-                indicator={antIcon} 
-                wrapperClassName='Loading_wrap' 
-                spinning={ROOT_loading}
+                size="large"
+                tip="Loading..."
+                indicator={<div className="spinningCircle"></div>} 
+                wrapperClassName="loading-wrap"
+                spinning={this.props.ROOT_loading}
             >
                 {this.props.children}
             </Spin>
